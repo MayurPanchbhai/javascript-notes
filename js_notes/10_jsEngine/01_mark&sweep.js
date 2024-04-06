88e33eaa3fa6543f1297e6a5cdb2dbc9edd247fe
MayurPanchbhai <mayurpanchbhai21@gmail.com>
2024-04-06 14:46:52 +0530

    js engine

---
+++ js_notes/10_jsEngine/01_mark&sweep.js
@@ -0,0 +1,4 @@
+// mark and sweep :-There are many garbage collection algorithms that run in the background, of which one of them is mark and sweep.
+
+
+// Javascript Runtime Environment :- A big container that has everything present that is required to run JS code.(like :- Web APIs , Callback Queue , microtask queue , eventlooop)
